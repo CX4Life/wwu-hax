aa3090bd88448ee20d95799f7d4d84e4b0a10760
Wyatt <wyatt@notexplosive.net>
2018-02-10 13:19:18 -0800

    Improved live object code, now able to delete safely

--- visualization/main.lua
+++ visualization/main.lua
@@ -9,19 +9,19 @@ INPUT_FILENAME = 'input.txt'
 -- Built in load function
 function love.load(args)
   INPUT_FILENAME = args[2]:split(' ')[1];
-  addLiveFunction({x=200,y=200},'draw',renderPiece)
+
+  print(unpack(LIVE_OBJECTS['draw']))
 end
 
 -- Built in update function
 -- Executes approximately every 1/60th of a second
 function love.update(dt)
-
+  runLiveFunction('update',dt)
 end
 
 -- Built in draw function
 -- Draws to the screen every 1/60th of a second
 function love.draw()
   love.graphics.print('hello world')
-  print(unpack(LIVE_OBJECTS['draw'][1]))
-  LIVE_OBJECTS['draw'][1]:draw()
+  runLiveFunction('draw')
 end
