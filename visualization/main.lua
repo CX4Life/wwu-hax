a69b1304d85019f582a3caa5bad99a88f67158b4
Wyatt <wyatt@notexplosive.net>
2018-02-10 15:50:08 -0800

    Fixed board size

--- visualization/main.lua
+++ visualization/main.lua
@@ -10,15 +10,28 @@ INPUT_FILENAME = 'input.txt'
 
 BOARD_SETTINGS = {
   offset = {
-    x = 100,
-    y = 32
+    x = 32,
+    y = 44
   },
   square = {
-    length = 54
+    length = 64
   }
 }
 
-Pawn()
+local id_counter = 0
+
+for y = 0,7 do
+  for x = 0,7 do
+    if x % 2 ~= y % 2 and y < 3 then
+      Pawn(id_counter,x,y)
+      id_counter = id_counter + 1
+    end
+    if x % 2 ~= y % 2 and y > 4 then
+      Pawn(id_counter,x,y)
+      id_counter = id_counter + 1
+    end
+  end
+end
 
 -- Built in load function
 function love.load(args)
