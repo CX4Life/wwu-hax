51502673eb4ccca822c6fb38d538c4b52687150c
tim woods <tim.woods.tw@gmail.com>
2018-02-10 14:13:43 -0800

    Basic CNN graph constructed

--- lstm.py
+++ lstm.py
@@ -22,13 +22,13 @@ def parse_args():
 def load_move_file(filename):
     return file_to_matrix.read_file(filename)
 
-def two_d_conv_layer(input, kernel_size, LPrime, stride, padding, func):
+def two_d_conv_layer(input, kernel_size, L, LPrime, stride, padding, func):
     """Create a 2d convolutional layer from parameters."""
     # expects a 4d tensor input of size MB x W x W x 1
     #TODO figure out why the first dimension is minibatch
     W = tf.get_variable(name="W",
                         shape=(kernel_size, kernel_size,
-                               1,
+                               L,
                                LPrime),
                         dtype=tf.float32,
                         initializer=tf.glorot_uniform_initializer()
@@ -46,7 +46,7 @@ def two_d_conv_layer(input, kernel_size, LPrime, stride, padding, func):
                         shape=(LPrime),
                         initializer=tf.constant_initializer(b_const)
                         )
-    z = tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding=padding)
+    z = tf.nn.conv2d(input, W, strides=[1, stride, stride, 1], padding=padding)
     with_bias = z + b
     return activation_function(with_bias)
 
@@ -60,18 +60,33 @@ def max_pooling_layer(input, kernel_size, stride):
 
 def conv_layer_with_max_pooling(prev_layer, conv_k, l, lprime, conv_s, pad,
                                 func, pool_k, pool_s):
-    conv_layer = two_d_conv_layer(prev_layer, conv_k, lprime, conv_s, pad, func)
+    conv_layer = two_d_conv_layer(prev_layer, conv_k, l, lprime, conv_s, pad, func)
     return max_pooling_layer(conv_layer, pool_k, pool_s)
 
 
-def build_graph(args, a0):
-    with tf.variable_scope("layer1_conv-max"):
-        a1 = conv_layer_with_max_pooling(a0, )
+def build_graph(args):
+    y_true = tf.placeholder(dtype=tf.int64, shape=(None), name="y_true")
+    x = tf.placeholder(dtype=tf.float32, shape=(None, 10, 10, 1), name='x')
 
+    with tf.variable_scope("con_max1"):
+        a1 = conv_layer_with_max_pooling(x, 3, 1, 16, 1, 'SAME', 'relu', 1, 1)
+    with tf.variable_scope("con_max2"):
+        a2 = conv_layer_with_max_pooling(a1, 3, 16, 16, 1, 'SAME', 'relu', 2, 2)
+    with tf.variable_scope("con_fc3"):
+        a3 = two_d_conv_layer(a2, 5, 16, 1, 1, 'VALID', 'relu')
+
+    z3 = tf.squeeze(a3)
+
+    cross_ent = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=z3, labels=y_true)
+    obj = tf.reduce_mean(cross_ent, name='obj')
+    train_step = tf.train.AdamOptimizer(args.lr).minimize(obj, name="train_step")
+    acc = tf.reduce_mean(tf.cast(tf.equal(y_true, tf.argmax(z3, axis=1)), tf.float32), name='acc')
+    init = tf.global_variables_initializer()
+    return init
 
 def main():
     args = parse_args()
-    print(args)
+    init = build_graph(args)
 
 
 if __name__ == '__main__':
