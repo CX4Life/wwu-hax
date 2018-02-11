90ac7781647a2d5df683d8386c4349c64fd7b6df
tim woods <tim.woods.tw@gmail.com>
2018-02-10 22:58:51 -0800

    Every w/l builds training set

--- np_from_wl.py
+++ np_from_wl.py
@@ -1,7 +1,8 @@
 import numpy as np
+import os
+from history_to_train import WIN_DIR, LOSS_DIR
 
-
-def np_array_from_line(filename_with_path):
+def np_array_from_filename(filename_with_path):
     ret = []
     long_array = np.genfromtxt(filename_with_path, dtype=float, delimiter=',')
     for array in long_array:
@@ -9,5 +10,19 @@ def np_array_from_line(filename_with_path):
     return ret
 
 
+def get_total_x_y():
+    every_x = []
+    every_y = []
+    for win_filename in os.listdir(WIN_DIR):
+        w_board_states = np_array_from_filename(win_filename)
+        every_x.extend(w_board_states)
+        every_y.extend([1.0] * len(w_board_states))
+    for loss_filename in os.listdir(LOSS_DIR):
+        l_board_states = np_array_from_filename(loss_filename)
+        every_x.extend(l_board_states)
+        every_y.extend([0.0] * len(l_board_states))
+    return every_x, every_y
+
+
 if __name__ == '__main__':
-    np_array_from_line('win_loss/loss/1518331391.631597loss.txt')
+    np_array_from_filename('win_loss/loss/1518331391.631597loss.txt')
