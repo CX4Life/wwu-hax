b2ea2831a637a085dac53e1299c9d9d55bc52bb7
notexplosive <thenotexplosive@gmail.com>
2018-02-11 02:56:05 -0800

    test?

--- visualization/main.lua
+++ visualization/main.lua
@@ -8,6 +8,7 @@ require('button')
 require('globals')
 require('progress_bar')
 require('ui')
+--
 
 love.window.setTitle('TensorFlow Plays Checkers')
 love.graphics.setBackgroundColor(kCOLOR_BACKGROUND)
