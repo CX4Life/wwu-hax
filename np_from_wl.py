7ab77b858ae0b4c891d36c09d55ee766f2139861
tim woods <tim.woods.tw@gmail.com>
2018-02-10 23:18:38 -0800

    cnn loads train and dev

--- np_from_wl.py
+++ np_from_wl.py
@@ -2,11 +2,15 @@ import numpy as np
 import os
 from history_to_train import WIN_DIR, LOSS_DIR
 
+
 def np_array_from_filename(filename_with_path):
     ret = []
     long_array = np.genfromtxt(filename_with_path, dtype=float, delimiter=',')
-    for array in long_array:
-        ret.append(array.reshape((8,8)))
+    if type(long_array[0]) is not np.float64:
+        for array in long_array:
+            ret.append(array.reshape((8,8)))
+    else:
+        ret.append(long_array.reshape((8,8)))
     return ret
 
 
@@ -14,13 +18,15 @@ def get_total_x_y():
     every_x = []
     every_y = []
     for win_filename in os.listdir(WIN_DIR):
-        w_board_states = np_array_from_filename(win_filename)
+        w_board_states = np_array_from_filename(WIN_DIR + win_filename)
         every_x.extend(w_board_states)
         every_y.extend([1.0] * len(w_board_states))
     for loss_filename in os.listdir(LOSS_DIR):
-        l_board_states = np_array_from_filename(loss_filename)
+        l_board_states = np_array_from_filename(LOSS_DIR + loss_filename)
         every_x.extend(l_board_states)
         every_y.extend([0.0] * len(l_board_states))
+
+    print('sanity check', len(every_x), len(every_y))
     return every_x, every_y
 
 
